bc8a4a2e423d1eaefa35e323bb967c81cf3d0517
Adriel da Silva <silvaadriel510@gmail.com>
2021-05-18 21:00:58 -0300

    Add TypeScript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,28 @@ import { View, Text, StyleSheet, TextInput, Platform, FlatList } from 'react-nat
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
+interface SkillData {
+  id: string;
+  name: string;
+}
+
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
   const [greeting, setGreeting] = useState('');
 
   function handleAddNewSkill() {
-    setMySkills((oldState) => [...oldState, newSkill]);
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    };
+
+    setMySkills((oldState) => [...oldState, data]);
+    setNewSkill('');
+  }
+
+  function handleRemoveSkill(id: string) {
+    setMySkills((oldState) => oldState.filter((skill) => skill.id !== id));
   }
 
   useEffect(() => {
@@ -30,17 +45,24 @@ export function Home() {
       <Text style={styles.title}>Welcome, User</Text>
       <Text style={styles.greeting}>{greeting}</Text>
 
-      <TextInput style={styles.input} placeholder='New skill' placeholderTextColor='#555' onChangeText={setNewSkill} />
+      <TextInput
+        style={styles.input}
+        value={newSkill}
+        placeholder='New skill'
+        placeholderTextColor='#555'
+        onChangeText={setNewSkill}
+      />
 
-      <Button onPress={handleAddNewSkill} />
+      <Button title='Add' onPress={handleAddNewSkill} disabled={!newSkill} />
 
       <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
 
       <FlatList
         data={mySkills}
-        keyExtractor={(_, index) => index}
-        renderItem={({ item }) => <SkillCard skill={item} />}
+        keyExtractor={({ id }) => id}
+        renderItem={({ item }) => <SkillCard skill={item.name} onPress={() => handleRemoveSkill(item.id)} />}
         showsVerticalScrollIndicator={false}
+        keyboardShouldPersistTaps='handled'
       />
     </View>
   );
